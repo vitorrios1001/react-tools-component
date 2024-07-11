0488a1efa21af69ec4ade35669ac1a812b937109
Vitor Rios <vr.vitorrios@gmail.com>
2024-07-11 15:09:05 -0300

    feat: create first component and publish on npm

---
+++ src/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface ButtonProps {
+  label: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({ label }) => {
+  return <button>{label}</button>;
+};
+
