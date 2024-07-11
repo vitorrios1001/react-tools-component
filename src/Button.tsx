a88f687a737a535d389eba8e6d5982ffe6137977
Vitor Rios <vr.vitorrios@gmail.com>
2024-07-11 15:16:23 -0300

    feat: add disable prop to Button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 export interface ButtonProps {
   label: string;
+  disabled?: boolean
 }
 
-export const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
+export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+  return <button {...props}>{label}</button>;
 };
 
