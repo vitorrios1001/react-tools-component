5ff74084ec182316ca9a3e994647581ca40f3f0a
Vitor Rios <vr.vitorrios@gmail.com>
2024-07-11 15:21:14 -0300

    feat: add loading prop to Button

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 export interface ButtonProps {
   label: string;
-  disabled?: boolean
+  disabled?: boolean;
+  loading?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
-  return <button {...props}>{label}</button>;
+export const Button: React.FC<ButtonProps> = ({ label, loading, ...props }) => {
+  return (
+    <button {...props} disabled={loading || props.disabled}>
+      {label}
+    </button>
+  );
 };
-
